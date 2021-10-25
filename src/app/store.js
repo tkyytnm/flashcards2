7d003d6efef536ba720457539d7447800748f088
Takuyay <mechanicalgiraffe@gmail.com>
2021-10-25 13:05:51 +0900

    First commit

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicsSlice from "../features/topics/topicsSlice";
+
+export default configureStore({
+  reducer: {
+    topicsSlice,
+    
+  },
+});
---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+  name: "topics",
+  initialState: { topics: {} },
+  reducers: {
+    addTopic: (state, action) => {
+      state = {
+        ...state,
+        [action.payload.id]: action.payload,
+        quizIds: []
+      };
+    }
+  }
+});
+
+export const selectTopics = (state) => state.topics.topics;
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
